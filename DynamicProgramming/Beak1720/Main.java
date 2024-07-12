8851dfaf6c2278521ccd5ad811c9f0bdb10cfc80
NAIMJAE <skdlawo5985@gamil.com>
2024-07-13 02:43:59 +0900

    0713

---
+++ DynamicProgramming/Beak1720/Main.java
@@ -0,0 +1,49 @@
+package DynamicProgramming.Beak1720;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ * 
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        // N이 홀수 / N이 짝수
+
+        // | 타일만 쓰는 경우 (홀짝 모두 가능)
+
+        // ㅡ 타일만 쓰는 경우 (짝만 가능)
+
+        // ㅁ 타일만 쓰는 경우 (짝만 가능)
+
+        // ㅁ | 타일을 쓰는 경우 (홀만 가능)
+
+        // ㅁ ㅡ 타일을 쓰는 경우 (짝만 가능)
+
+        // ㅁ | ㅡ 타일 모두 쓰는 경우 (홀만 가능)
+
+        int count = 0;
+
+        // case1
+        count += N;
+
+        if (N%2 == 0) {
+            // case2
+            count += N;
+            // case3
+            count += N/2;
+            // case5
+            if (N >= 4) {
+                //2-0, 4-1, 6-3, 8-5, 10-8, 12-8
+            }
+            count += N/2;
+            
+        }
+
+
+    }
+}
