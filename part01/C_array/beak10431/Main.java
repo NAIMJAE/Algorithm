05e8b9d865b3cd077e0b870f4e2caa5b2d547008
naimjae <skdlawo5985@gmail.com>
2026-01-12 23:24:11 +0900

    260112

--- part01/C_array/beak10431/Main.java
+++ part01/C_array/beak10431/Main.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 /**
  * 백준 10431번 - 줄세우기
  * https://www.acmicpc.net/problem/10431
+ * - 전체 시간 복잡도: O(P * N^2) (P: 테스트 케이스 수, N: 학생 수 20명)
+ * 
+* - 각 학생이 줄을 설 때마다 '자기 앞에 서 있는 모든 학생'과 비교함
+ * - 문제에서 N을 20으로 제한했으나, 내부 반복문의 횟수가 고정 상수가 아니라 
+ *   N의 크기에 종속되어 가변적으로 증가하는 '삽입 정렬' 구조이므로 O(N^2)임
+ * - N이 작기 때문에(20) O(N^2) 로직으로도 시간 제한 내에 충분히 해결 가능함
  */
 public class Main {
     public static void main(String[] args) {
